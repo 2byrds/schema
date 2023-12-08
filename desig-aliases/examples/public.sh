3a74314dd25c70ba0284c4e44b205f3c82d7bced
2byrds <2byrds@gmail.com>
2023-12-07 19:20:34 -0500

    improved automated credential creation and successfully generated did:webs from it
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- desig-aliases/examples/public.sh
+++ desig-aliases/examples/public.sh
@@ -10,44 +10,39 @@ d_alias_schema="EN6Oh5XSD5_q2Hgu-aqpdfbVepdpYpFlgz6zvJL5b_r5"
 da_schema_oobi="https://weboftrust.github.io/oobi/${d_alias_schema}"
 
 #clear state from previous runs
+echo "Clearing state from previous runs for $alias"
 find /usr/local/var/keri/* -name "$alias" -type d -exec rm -rf {} + 2>/dev/null
+find /usr/local/var/keri/* -name "$reg_name" -type d -exec rm -rf {} + 2>/dev/null
 
-kli init --name "$alias" --salt 0AAQmsjh-C7kAJZQEzdrzwB7 --nopasscode --config-dir "./my-scripts" --config-file my-config
+echo "Initializing $alias"
+kli init --name "$alias" --salt 0AAQmsjh-C7kAJZQEzdrzwB7 --nopasscode --config-dir "./my-scripts" --config-file config
+echo "Incepting $alias"
 kli incept --name "$alias" --alias "$alias" --file "./my-scripts/my-incept.json"
+echo "Getting $alias status"
 kli status --name "$alias" --alias "$alias"
 
+echo "Creating registry $reg_name"
 kli vc registry incept --name "$alias" --alias "$alias" --registry-name "$reg_name"
 
+echo "Saidifying attestation rules"
 kli saidify --file ./desig-aliases-rules-public.json --label "d"
+echo "Saidifying attestation attributes"
 kli saidify --file ./desig-aliases-attr-public.json --label "d"
 
 # manually add rules example SAID and attribute example SAID to the desig-aliases.json
-read -p "Hit enter after you have added the registry SAID (and maybe attrs, rules, etc) to desig-aliases-public.json"
-kli saidify --file ./desig-aliases-public.json --label "d"
+# read -p "Hit enter after you have added the registry SAID (and maybe attrs, rules, etc) to desig-aliases-public.json"
+# echo "Saidifying attestation with the registry you added"
+# kli saidify --file ./desig-aliases-public.json --label "d"
 
+echo "Resolving schema at $da_schema_oobi"
 kli oobi resolve --name "$alias" --oobi-alias myDesigAliases --oobi "$da_schema_oobi"
-kli vc create --name "$alias" --alias "$alias" --registry-name "$reg_name" --schema "${d_alias_schema}" --credential @desig-aliases-public.json
+echo "Issuing attestation to registry $reg_name with schema $d_alias_schema from credential file desig-aliases-public.json"
+kli vc create --name "$alias" --alias "$alias" --registry-name "$reg_name" --schema "${d_alias_schema}" --data @desig-aliases-attr-public.json --rules @desig-aliases-rules-public.json
+# kli vc create --name "$alias" --alias "$alias" --registry-name "$reg_name" --schema "${d_alias_schema}" --credential @desig-aliases-public.json
+echo "Getting SAID for attestation in registry $reg_name with schema $d_alias_schema"
+SAID=$(kli vc list --name "$alias" --alias "$alias" --issued --said --schema "${d_alias_schema}")
+echo "Listing attestations in registry $reg_name with schema $d_alias_schema"
 kli vc list --name "$alias" --alias "$alias" --issued --schema "${d_alias_schema}"
 
-SAID=$(kli vc list --name "$alias" --alias "$alias" --issued --said --schema "${d_alias_schema}")
+echo "Exporting attestation with SAID $SAID"
 kli vc export --name "$alias" --alias "$alias" --said "${SAID}" --chain
-# SAID=$(kli vc list --name myself --alias myself --issued --said --schema EPi_tVS3zDN4wo-T3NQb5pUtdeup98yoJ_hrNReD86xO)
-# echo "SAID of the Authorized Identifiers attestation is: ${SAID}"
-
-# # kli vc create --name myself --alias myself --registry-name vLEI --schema EBfdlu8R27Fbx-ehrqwImnK-8Cm79sqbAQ4MmvEAYqao --recipient ELjSFdrTdCebJlmvbFNX9-TLhR2PO0_60al1kQp5_e6k --data @${KERI_DEMO_SCRIPT_DIR}/data/credential-data.json
-# # SAID=$(kli vc list --name myself --alias myself --issued --said --schema EBfdlu8R27Fbx-ehrqwImnK-8Cm79sqbAQ4MmvEAYqao)
-
-# # kli ipex grant --name myself --alias myself --said "${SAID}" --recipient ELjSFdrTdCebJlmvbFNX9-TLhR2PO0_60al1kQp5_e6k
-
-# # echo "Checking holder for grant messages..."
-# # GRANT=$(kli ipex list --name holder --alias holder --poll --said)
-
-# # echo "Admitting credential from grant ${GRANT}"
-# # kli ipex admit --name holder --alias holder --said "${GRANT}"
-
-# kli vc list --name myself --alias myself
-# exit 0
-
-# kli vc revoke --name myself --alias myself --registry-name authId --said "${SAID}"
-# sleep 2
-# kli vc list --name myself --alias myself --poll
